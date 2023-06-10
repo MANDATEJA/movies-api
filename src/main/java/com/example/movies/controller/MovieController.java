a2cf2022d9e5b129dd949ae8a3d7a85cf924754b
Tejareddy <mandateja@Tejareddys-MacBook-Pro.local>
2023-06-10 10:55:58 -0500

    fix: add cors server config to allow requests from local development instance

--- src/main/java/com/example/movies/controller/MovieController.java
+++ src/main/java/com/example/movies/controller/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.example.movies.dto.Movie;
 import com.example.movies.service.MovieService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
   @Autowired
