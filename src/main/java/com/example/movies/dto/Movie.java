fc2b050d99741d76264cdb992467172f2699fe83
Tejareddy <mandateja@Tejareddys-MacBook-Pro.local>
2023-06-11 10:03:21 -0500

    fix: trailer field in Movie data object

--- src/main/java/com/example/movies/dto/Movie.java
+++ src/main/java/com/example/movies/dto/Movie.java
@@ -22,7 +22,7 @@ public class Movie {
   private String imdbId;
   private String title;
   private String releaseDate;
-  private String trilerLink;
+  private String trailerLink;
   private String poster;
   private List<String> genres;
   private List<String>backdrops;
